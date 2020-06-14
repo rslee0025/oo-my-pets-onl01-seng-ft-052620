df5f238e934fd5b0d17cc97c3d99f0a4043d0c07
Rachel S. Lee <rslee0025@gmail.com>
2020-06-14 03:06:45 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,44 +1,73 @@
-require_relative ('cat')
-
 class Owner
+  # code goes here	
   attr_reader :name, :species
-  
-  @@all = []     #all the owners
-  
+
+  @@owner = []
+
   def initialize(name)
-    @name = name 
+    @name = name
+    @@owner << self
     @species = "human"
-    @@all << self
-    @pets = {cats: [], dogs: []}
-    
-  end
-  
-  def say_species
-    "I am a #{species}."
   end
 
-  def self.all 
-    @@owners
+  def say_species
+    @self = "I am a human."
+  end
+
+  def self.all
+    @@owner
   end
 
   def self.count
-    self.all.count
+    @@owner.size
   end
-  
+
   def self.reset_all
-    self.all.clear
+    @@owner.clear
   end
 
-
-  def self.all
-    @@all
+  def cats 
+    owned_cat = Cat.all.select {|cat| cat.owner == self}
+    owned_cat
   end
-   
-   def cats
-      @pets.map { |cats:| Cat.name == Owner.cats }
-   end
-   
 
- 
- 
-end
+  def dogs
+    owned_dog = Dog.all.select {|dog| dog.owner == self}
+    owned_dog
+  end
+
+  def buy_cat(cat)
+    Cat.new(cat, self)
+  end
+
+  def buy_dog(dog)
+    Dog.new(dog, self)
+  end
+
+  def walk_dogs
+    Dog.all.each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def feed_cats
+    Cat.all.each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    Cat.all.each do |cat|
+      cat.owner = nil
+      cat.mood = "nervous"
+    end
+    Dog.all.each do |dog|
+      dog.owner = nil
+      dog.mood = "nervous"
+    end
+  end
+
+  def list_pets
+
+    "I have #{dogs.length} dog(s), and #{cats.length} cat(s)."
+  end
